8f1693100d759e0d3b36bb3335282f14f7a5c47e
Thijmen <Thijmenhensbergen@gmail.com>
2026-08-23 23:03:19 +0200

    Started work on Minafara
    Updated License
    - Now a modified MIT license that disallows AI training, Discrimination and Military purposes (not that it will happen but just in case of a 1/1000000 chance yknow)
    Added Reality.cs
    - Only holds a simple entity list for now
    Added Entity.cs
    - Has a name, Children List and a couple functions to modify it.

---
+++ MinafaraECS/MinafaraECS/Source/Entity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinafaraECS
+{
+    public class Entity
+    {
+        public string Name = "";
+        private List<Entity> Children = [];
+
+
+        public void AddChild(Entity Child)
+        {
+            Children.Add(Child);
+        }
+        public void RemoveChild(string EntityName)
+        {
+            Entity? Child = Children.FirstOrDefault(e => e.Name == EntityName);
+            if (Child != null)
+            {
+                Children.Remove(Child);
+            }
+        }
+        public void RemoveChildren(string EntityName, bool CaseSensitive = true)
+        {
+            foreach (var Child in Children)
+            {
+                if (CaseSensitive)
+                {
+                    if (Child.Name.Contains(EntityName))
+                    {
+                        Children.Remove(Child);
+                    }
+                } else {
+                    if (Child.Name.Contains(EntityName, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        Children.Remove(Child);
+                    }
+                }
+
+            }
+        }
+
+    }
+}
---
+++ MinafaraECS/MinafaraECS/Source/Reality.cs
@@ -0,0 +1,13 @@
+﻿namespace MinafaraECS
+{
+    public class Reality
+    {
+        private List<Entity> Entities = new();
+
+        public List<Entity> GetEntities()
+        {
+            return Entities;
+        }
+
+    }
+}
